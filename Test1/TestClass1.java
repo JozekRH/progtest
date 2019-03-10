26db41eb41f47082ade30791a7fcefdd167b0c58
JozekRH <kolybaev@gmail.com>
2019-03-10 21:04:40 +0200

    Test1

--- Test1/TestClass1.java
+++ Test1/TestClass1.java
@@ -14,8 +14,8 @@ public class TestClass1{
 	public void runTest()
 	{
 		Fibonacci f = new Fibonacci();
-		Assert.assertTrue("Тест dynamic не прошел", f.dynamic(12001).toString().equals(""));
-		Assert.assertEquals(f.matrix(12001).toString(), testRightResult);
+		Assert.assertTrue("Тест dynamic не прошел", f.dynamic(12001).toString().equals(testRightResult));
+		Assert.assertTrue("Тест matrix не прошел", f.matrix(12001).toString().equals(testRightResult));
 	}
 	
 	public static TestResult processResult(Result jUnitResult)
