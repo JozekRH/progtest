b2bbc71947bc31f0f046a2dddd65f2376ba64daa
JozekRH <kolybaev@gmail.com>
2019-03-20 21:08:11 +0200

    Test1

--- Test1/TestClass1.java
+++ Test1/TestClass1.java
@@ -36,6 +36,9 @@ public class TestClass1{
 	{
 		TestTask21 tt = new TestTask21();
 		Assert.assertTrue(tt.t1().equals("111"));
+		TestTask22 tt = new TestTask22();
+		Assert.assertTrue(tt.t1().equals("111"));
+		Assert.assertTrue(tt.t.equals("111"));
 	}
 	
 	@Test
