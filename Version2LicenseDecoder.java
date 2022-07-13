150cce768a2abef9661ede312bdca425a18c107d
JozekRH <47397991+JozekRH@users.noreply.github.com>
2022-07-13 22:26:55 +0200

    Add files via upload

--- Version2LicenseDecoder.java
+++ Version2LicenseDecoder.java
@@ -10,12 +10,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
+
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
-import java.security.Signature;
-import java.security.SignatureException;
+
+
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Properties;
@@ -36,209 +36,184 @@ public class Version2LicenseDecoder
   private static final PublicKey PUBLIC_KEY;
   private static final int ENCODED_LICENSE_LINE_LENGTH = 76;
 
+
   static  {
     try {
-      pubKeyEncoded = "MIIBuDCCASwGByqGSM44BAEwggEfAoGBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAhUAl2BQjxUjC8yykrmCouuEC/BYHPUCgYEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoDgYUAAoGBAIvfweZvmGo5otwawI3no7Udanxal3hX2haw962KL/nHQrnC4FG2PvUFf34OecSK1KtHDPQoSQ+DHrfdf6vKUJphw0Kn3gXm4LS8VK/LrY7on/wh2iUobS2XlhuIqEc5mLAUu9Hd+1qxsQkQ50d0lzKrnDqPsM0WA9htkdJJw2nS";
-
+      String str = "MIIBuDCCASwGByqGSM44BAEwggEfAoGBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAhUAl2BQjxUjC8yykrmCouuEC/BYHPUCgYEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoDgYUAAoGBAIvfweZvmGo5otwawI3no7Udanxal3hX2haw962KL/nHQrnC4FG2PvUFf34OecSK1KtHDPQoSQ+DHrfdf6vKUJphw0Kn3gXm4LS8VK/LrY7on/wh2iUobS2XlhuIqEc5mLAUu9Hd+1qxsQkQ50d0lzKrnDqPsM0WA9htkdJJw2nS";
 
       KeyFactory keyFactory = KeyFactory.getInstance("DSA");
       PUBLIC_KEY = keyFactory.generatePublic(new X509EncodedKeySpec(Base64.decodeBase64("MIIBuDCCASwGByqGSM44BAEwggEfAoGBAP1/U4EddRIpUt9KnC7s5Of2EbdSPO9EAMMeP4C2USZpRV1AIlH7WT2NWPq/xfW6MPbLm1Vs14E7gB00b/JmYLdrmVClpJ+f6AR7ECLCT7up1/63xhv4O1fnxqimFQ8E+4P208UewwI1VBNaFpEy9nXzrith1yrv8iIDGZ3RSAHHAhUAl2BQjxUjC8yykrmCouuEC/BYHPUCgYEA9+GghdabPd7LvKtcNrhXuXmUr7v6OuqC+VdMCz0HgmdRWVeOutRZT+ZxBxCBgLRJFnEj6EwoFhO3zwkyjMim4TwWeotUfI0o4KOuHiuzpnWRbqN/C/ohNWLx+2J6ASQ7zKTxvqhRkImog9/hWuWfBpKLZl6Ae1UlZAFMO/7PSSoDgYUAAoGBAIvfweZvmGo5otwawI3no7Udanxal3hX2haw962KL/nHQrnC4FG2PvUFf34OecSK1KtHDPQoSQ+DHrfdf6vKUJphw0Kn3gXm4LS8VK/LrY7on/wh2iUobS2XlhuIqEc5mLAUu9Hd+1qxsQkQ50d0lzKrnDqPsM0WA9htkdJJw2nS".getBytes())));
     }
-    catch (NoSuchAlgorithmException e) {
-      throw new Error(e);
+    catch (NoSuchAlgorithmException noSuchAlgorithmException) {
+
+      throw new Error(noSuchAlgorithmException);
     }
-    catch (InvalidKeySpecException e) {
+    catch (InvalidKeySpecException invalidKeySpecException) {
 
-
-      throw new Error(e);
+      throw new Error(invalidKeySpecException);
     }
   }
 
-  public boolean canDecode(String licenseString) {
-    licenseString = removeWhiteSpaces(licenseString);
+  public boolean canDecode(String paramString) {
+    paramString = removeWhiteSpaces(paramString);
 
-    int pos = licenseString.lastIndexOf('X');
-    if (pos == -1 || pos + 3 >= licenseString.length())
+    int i = paramString.lastIndexOf('X');
+    if (i == -1 || i + 3 >= paramString.length())
     {
       return false;
     }
 
+
     try {
-      int version = Integer.parseInt(licenseString.substring(pos + 1, pos + 3));
-      if (version != 1 && version != 2)
+      int j = Integer.parseInt(paramString.substring(i + 1, i + 3));
+      if (j != 1 && j != 2)
       {
         return false;
       }
 
-      String lengthStr = licenseString.substring(pos + 3);
-      int encodedLicenseLength = Integer.valueOf(lengthStr, 31).intValue();
-      if (pos != encodedLicenseLength)
+      String str = paramString.substring(i + 3);
+      int k = Integer.valueOf(str, 31).intValue();
+      if (i != k)
       {
         return false;
       }
 
       return true;
     }
-    catch (NumberFormatException e) {
+    catch (NumberFormatException numberFormatException) {
 
       return false;
     }
   }
 
-  public Properties doDecode(String licenseString) {
-    String encodedLicenseTextAndHash = getLicenseContent(removeWhiteSpaces(licenseString));
-    byte[] zippedLicenseBytes = checkAndGetLicenseText(encodedLicenseTextAndHash);
-    Reader licenseText = unzipText(zippedLicenseBytes);
+  public Properties doDecode(String paramString) {
+    String str = getLicenseContent(removeWhiteSpaces(paramString));
+    byte[] arrayOfByte = checkAndGetLicenseText(str);
+    Reader reader = unzipText(arrayOfByte);
 
-    return loadLicenseConfiguration(licenseText);
+    return loadLicenseConfiguration(reader);
   }
 
   protected int getLicenseVersion() { return 2; }
 
-  private Reader unzipText(byte[] licenseText) {
-    ByteArrayInputStream in = new ByteArrayInputStream(licenseText);
-    in.skip(LICENSE_PREFIX.length);
-    InflaterInputStream zipIn = new InflaterInputStream(in, new Inflater());
+  private Reader unzipText(byte[] paramArrayOfByte) {
+    ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(paramArrayOfByte);
+    byteArrayInputStream.skip(LICENSE_PREFIX.length);
+    InflaterInputStream inflaterInputStream = new InflaterInputStream(byteArrayInputStream, new Inflater());
 
     try {
-      return new InputStreamReader(zipIn, "UTF-8");
+      return new InputStreamReader(inflaterInputStream, "UTF-8");
     }
-    catch (UnsupportedEncodingException e) {
+    catch (UnsupportedEncodingException unsupportedEncodingException) {
 
-
-      throw new LicenseException(e);
+      throw new LicenseException(unsupportedEncodingException);
     }
   }
 
-  private String getLicenseContent(String licenseString) {
-    String lengthStr = licenseString.substring(licenseString.lastIndexOf('X') + 3);
+  private String getLicenseContent(String paramString) {
+    String str = paramString.substring(paramString.lastIndexOf('X') + 3);
 
     try {
-      int encodedLicenseLength = Integer.valueOf(lengthStr, 31).intValue();
-      return licenseString.substring(0, encodedLicenseLength);
+      int i = Integer.valueOf(str, 31).intValue();
+      return paramString.substring(0, i);
     }
-    catch (NumberFormatException e) {
+    catch (NumberFormatException numberFormatException) {
 
-      throw new LicenseException("Could NOT decode license length <" + lengthStr + ">", e);
+      throw new LicenseException("Could NOT decode license length <" + str + ">", numberFormatException);
     }
   }
 
-  private byte[] checkAndGetLicenseText(String licenseContent) {
-    byte[] licenseText;
+  private byte[] checkAndGetLicenseText(String paramString) {
+    byte[] arrayOfByte;
     try {
-      byte[] decodedBytes = Base64.decodeBase64(licenseContent.getBytes());
-      ByteArrayInputStream in = new ByteArrayInputStream(decodedBytes);
-      DataInputStream dIn = new DataInputStream(in);
-      int textLength = dIn.readInt();
-      licenseText = new byte[textLength];
-      dIn.read(licenseText);
-      byte[] hash = new byte[dIn.available()];
-      dIn.read(hash);
-
-      try {
-        Signature signature = Signature.getInstance("SHA1withDSA");
-        signature.initVerify(PUBLIC_KEY);
-        signature.update(licenseText);
-        if (!signature.verify(hash))
-        {
-          throw new LicenseException("Failed to verify the license.");
-        }
-      }
-      catch (InvalidKeyException e) {
-        throw new LicenseException(e);
-      }
-      catch (SignatureException e) {
-
-        throw new LicenseException(e);
-      }
-      catch (NoSuchAlgorithmException e) {
+      byte[] arrayOfByte1 = Base64.decodeBase64(paramString.getBytes());
+      ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(arrayOfByte1);
+      DataInputStream dataInputStream = new DataInputStream(byteArrayInputStream);
+      int i = dataInputStream.readInt();
+      arrayOfByte = new byte[i];
+      dataInputStream.read(arrayOfByte);
+      byte[] arrayOfByte2 = new byte[dataInputStream.available()];
+      dataInputStream.read(arrayOfByte2);
 
 
-        throw new LicenseException(e);
-      }
+    }
+    catch (IOException iOException) {
 
-    } catch (IOException e) {
-
-
-      throw new LicenseException(e);
+      throw new LicenseException(iOException);
     }
 
-    return licenseText;
+    return arrayOfByte;
   }
 
-  private Properties loadLicenseConfiguration(Reader text) {
+  private Properties loadLicenseConfiguration(Reader paramReader) {
     try {
-      Properties props = new Properties();
-      (new DefaultPropertiesPersister()).load(props, text);
-      return props;
+      Properties properties = new Properties();
+      (new DefaultPropertiesPersister()).load(properties, paramReader);
+      return properties;
     }
-    catch (IOException e) {
+    catch (IOException iOException) {
 
-      throw new LicenseException("Could NOT load properties from reader", e);
+      throw new LicenseException("Could NOT load properties from reader", iOException);
     }
   }
 
-  private static String removeWhiteSpaces(String licenseData) {
-    if (licenseData == null || licenseData.length() == 0)
+  private static String removeWhiteSpaces(String paramString) {
+    if (paramString == null || paramString.length() == 0)
     {
-      return licenseData;
+      return paramString;
     }
 
-    char[] chars = licenseData.toCharArray();
-    StringBuffer buf = new StringBuffer(chars.length);
-    for (int i = 0; i < chars.length; i++) {
+    char[] arrayOfChar = paramString.toCharArray();
+    StringBuffer stringBuffer = new StringBuffer(arrayOfChar.length);
+    for (byte b = 0; b < arrayOfChar.length; b++) {
 
-      if (!Character.isWhitespace(chars[i]))
+      if (!Character.isWhitespace(arrayOfChar[b]))
       {
-        buf.append(chars[i]);
+        stringBuffer.append(arrayOfChar[b]);
       }
     }
 
-    return buf.toString();
+    return stringBuffer.toString();
   }
 
-  public static String packLicense(byte[] text, byte[] hash) throws LicenseException {
+  public static String packLicense(byte[] paramArrayOfByte1, byte[] paramArrayOfByte2) throws LicenseException {
     try {
-      ByteArrayOutputStream out = new ByteArrayOutputStream();
-      DataOutputStream dOut = new DataOutputStream(out);
-      dOut.writeInt(text.length);
-      dOut.write(text);
-      dOut.write(hash);
+      ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+      DataOutputStream dataOutputStream = new DataOutputStream(byteArrayOutputStream);
+      dataOutputStream.writeInt(paramArrayOfByte1.length);
+      dataOutputStream.write(paramArrayOfByte1);
+      dataOutputStream.write(paramArrayOfByte2);
 
-      byte[] allData = out.toByteArray();
-      result = (new String(Base64.encodeBase64(allData))).trim();
+      byte[] arrayOfByte = byteArrayOutputStream.toByteArray();
+      String str = (new String(Base64.encodeBase64(arrayOfByte))).trim();
 
-
-
-
-      result = result + 'X' + "0" + '\002' + Integer.toString(result.length(), 31);
-      return split(result);
-
+      str = str + 'X' + "0" + '\002' + Integer.toString(str.length(), 31);
+      return split(str);
     }
-    catch (IOException e) {
+    catch (IOException iOException) {
 
-
-      throw new LicenseException(e);
+      throw new LicenseException(iOException);
     }
   }
 
-  private static String split(String licenseData) {
-    if (licenseData == null || licenseData.length() == 0)
+  private static String split(String paramString) {
+    if (paramString == null || paramString.length() == 0)
     {
-      return licenseData;
+      return paramString;
     }
 
-    char[] chars = licenseData.toCharArray();
-    StringBuffer buf = new StringBuffer(chars.length + chars.length / 76);
-    for (int i = 0; i < chars.length; i++) {
+    char[] arrayOfChar = paramString.toCharArray();
+    StringBuffer stringBuffer = new StringBuffer(arrayOfChar.length + arrayOfChar.length / 76);
+    for (byte b = 0; b < arrayOfChar.length; b++) {
 
-      buf.append(chars[i]);
-      if (i > 0 && i % 76 == 0)
+      stringBuffer.append(arrayOfChar[b]);
+      if (b && b % 76 == 0)
       {
-        buf.append('\n');
+        stringBuffer.append('\n');
       }
     }
 
-    return buf.toString();
+    return stringBuffer.toString();
   }
+
 }
